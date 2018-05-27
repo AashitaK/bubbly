110f6ff0d08596517b6a81759593c918b8f3b6c3
Aashita Kesarwani <kesar01@gmail.com>
2018-05-27 09:16:13 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as file:
+        return file.read()
+
+setup(name='bubbles',
+      version='0.1',
+      description='A module for plotting interactive and animated bubble charts using Plotly',
+      long_description=readme(),
+      url='https://github.com/AashitaK/nyt-comments',
+      keywords=['Plotly', 'bubble charts', 'animated graphs', 'interactive graphs'],
+      author='Aashita Kesarwani',
+      author_email='kesar01@gmail.com',
+      license='MIT',
+      packages=['bubbles'],
+      install_requires=[
+          'plotly',
+          'pandas',
+      ],
+      zip_safe=False)
